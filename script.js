d4766729c5e65d4f4004127eaa97c17695aaa8bf
Ian Toy <iet@ela.com>
2019-12-09 15:32:20 -0800

    began work on current date display, copied template from stack overflow, adapting for use

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-// This is the basic outline for the day planner homework
-
 // The top of the calendar will display
 
     // the current day, and time if I can swing it
+var currentDay = document.querySelector("#currentDay");
+
+
+// below is an example copied from stack overflow. Take what you need from here and port it over to the index.html as necessary
+var stackOverflowTemp = document.querySelector("#stackOverflowTemp");
+
+var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours());
+    
+var today = dayOfWeek + ", " + curMonth + " " + dayOfMonth + ", " + curYear;
+
+// document.getElementsByTagName('h1')[0].textContent = today;
+
+currentDay.textContent = today;
+
+// below is what I need to glean from the above StackOverflow Example
+var weekdayArr;
+weekdayArr = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+var monthsArr;
+monthsArr = ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+
+
+
 
 // display standard business hours from 9am to 5pm
 
@@ -25,5 +53,10 @@
     // a save button to the right of each time slot
     
         // clicking on this save button saves the user input to localStorage
+
+
+function saveEvent() {
+
+}
 
 
