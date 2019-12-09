0fbc1d2361cd17a72c555243740355fb156496d9
Ian Toy <iet@ela.com>
2019-12-08 16:01:31 -0800

    initialized repo, pseudocoded assignment objectives, created js and scratchwork files

---
+++ script.js
@@ -0,0 +1,29 @@
+// This is the basic outline for the day planner homework
+
+// The top of the calendar will display
+
+    // the current day, and time if I can swing it
+
+// display standard business hours from 9am to 5pm
+
+// each time slot should represent one hour and contain
+
+    // the time, on the far left
+
+    // a field to hold user imput
+    
+        // this background will change based on the time of day,
+        
+        // indicating wether or not this time has passed
+        
+        // use this to indicate also if the event is current or upcoming.
+        
+        // also change based on wheter the hour is free or filled.
+        
+        // use the Moment.js library for this, read docs carefully
+    
+    // a save button to the right of each time slot
+    
+        // clicking on this save button saves the user input to localStorage
+
+
